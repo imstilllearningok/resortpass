83e00e99fd8b521b98030dcaa6f14cacce45702b
Randy Richardson <rmr.randy@gmail.com>
2025-03-26 18:51:25 -0400

    new

--- Introduction.py
+++ Introduction.py
@@ -17,7 +17,7 @@ st.markdown("**The tools I used:**")
 lst = ['Streamlit - for the web application', 
        'Google Cloud Platform (BigQuery) - for the data warehouse',
        'DBT - for data transformation',
-       'Python (various packages) - for data manipulation, analysis, visualization, and modeling',
+       'Python (various packages) - for data analysis, visualization, and modeling',
        'GitHub - for version control']
 s = ''
 
